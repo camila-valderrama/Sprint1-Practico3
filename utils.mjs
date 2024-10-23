99024f0a121878fd9702d4fa9f866dff5b317062
camila-valderrama <camival00@gmail.com>
2024-10-23 19:29:23 -0300

    first commit

---
+++ utils.mjs
@@ -0,0 +1,43 @@
+import fs from 'fs';
+
+//Clase para representar un superhéroe
+
+class Superheroe {
+    constructor(id, nombreSuperheroe, nombreReal, nombreSociedad, edad, 
+                planetaOrigen, debilidad, poder, habilidadEspecial, aliado,
+                enemigo){
+    this.id = id;
+    this.nombreSuperheroe = nombreSuperheroe;
+    this.nombreReal = nombreReal;
+    this.nombreSociedad = nombreSociedad;
+    this.edad = edad;
+    this.planetaOrigen = planetaOrigen;
+    this.debilidad = debilidad;
+    this.poder = poder;
+    this.habilidadEspecial = habilidadEspecial;
+    this.aliado = aliado;
+    this.enemigo = enemigo;                
+    }
+}
+
+//Funcion para leer y ordenar los superhéroes
+
+export function leerSuperheroes(ruta) {
+    const datos = fs.readFileSync(ruta, 'utf8');
+    const superheroesArray = JSON.parse(datos);
+
+    //Convertir a instancias de Superhéroe
+
+    const superheroes = superheroesArray.map(
+    hero => new Superheroe(hero.id, hero.nombreSuperheroe, hero.nombreReal, 
+            hero.nombreSociedad, hero.edad, hero.planetaOrigen, hero.debilidad,
+            hero.poder, hero.habilidadEspecial, hero.aliado, hero.enemigo)
+    );
+
+    //Ordenar por nombre de superhéroe
+
+    superheroes.sort((a, b) => a.nombreSuperheroe.localeCompare(
+        b.nombreSuperheroe));
+    return superheroes;
+};
+
