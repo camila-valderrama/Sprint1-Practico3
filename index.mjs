7c8b52670f2dcef4a31c4a963b429254ff0f6d29
camila-valderrama <camival00@gmail.com>
2024-10-24 13:49:13 -0300

    terminando el practico

--- index.mjs
+++ index.mjs
@@ -1,7 +1,13 @@
-import { leerSuperheroes } from "./utils.mjs";
+import { leerSuperheroes, agregarSuperheroes } from "./utils.mjs";
+
+const archivoOriginal = './superheroes.txt';
+const archivoNuevos = './agregarSuperheroes.txt';
+
+//Agregar nuevo superhéroes
+agregarSuperheroes(archivoOriginal, archivoNuevos);
 
 //Leer y mostrar la lista de superhéroes ordenada
-const superheroes = leerSuperheroes('./superheroes.txt');
+const superheroes = leerSuperheroes(archivoOriginal);
 console.log('Superhéroes ordenados:');
 console.log(superheroes);
 
